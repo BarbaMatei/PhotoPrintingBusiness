7590446bd35ce458298c7c879c9d94f895180e50
Matei Barba <matei.barba@tss-yonder.com>
2026-05-27 10:10:15 +0300

    fix: removed upload folder

--- src/PhotoPrint.API/obj/Debug/net8.0/PhotoPrint.API.AssemblyInfo.cs
+++ src/PhotoPrint.API/obj/Debug/net8.0/PhotoPrint.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhotoPrint.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bdd2db16241fe6be0ba1746dc77b464a94ccf3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhotoPrint.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhotoPrint.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
