4ebee1e4d85183a4751566af1e4056f4eb0e9b5f
Matei Barba <matei.barba@tss-yonder.com>
2026-09-04 11:42:23 +0300

    refactor(account): split the profile and saved-addresses pages into container plus form components (bolt 067, story 003)

--- src/PhotoPrint.UI/src/app/features/account/account.routes.ts
+++ src/PhotoPrint.UI/src/app/features/account/account.routes.ts
@@ -3,17 +3,17 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pages/account-layout').then(m => m.AccountLayout),
+    loadComponent: () => import('./pages/account-layout').then((m) => m.AccountLayout),
     children: [
       { path: '', redirectTo: 'profil', pathMatch: 'full' },
       {
         path: 'profil',
-        loadComponent: () => import('./pages/profile/profile-page').then(m => m.ProfilePage),
+        loadComponent: () => import('./pages/profile/profile-page').then((m) => m.ProfilePage),
       },
       {
         path: 'adrese',
         loadComponent: () =>
-          import('./pages/saved-addresses/saved-addresses-page').then(m => m.SavedAddressesPage),
+          import('./pages/saved-addresses/saved-addresses-page').then((m) => m.SavedAddressesPage),
       },
     ],
   },
